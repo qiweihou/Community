e42019042b2ddd03d05c4fe835e9d9549464870a
qiwei_hou@163.com <qiwei_hou@163.com>
2020-01-07 20:57:14 +0800

    init repo

---
+++ src/main/java/com/qiweihou/community/Controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.qiweihou.community.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author qiwei
+ * @date 2020/1/5
+ */
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello";
+    }
+}
