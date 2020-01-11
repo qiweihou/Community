b64273fa3aaaab4dfbac45b501f7e1968cd5b48a
houqiwei <qiwei_hou@163.com>
2020-01-11 10:50:05 +0800

    github登录授权

---
+++ src/main/java/com/qiweihou/community/provider/GithubProvider.java
@@ -0,0 +1,51 @@
+package com.qiweihou.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import com.qiweihou.community.dto.AccessTokenDTO;
+import com.qiweihou.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+/**
+ * @author qiwei
+ * @date 2020/1/10
+ */
+@Component
+public class GithubProvider {
+
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDTO));
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String string = response.body().string();
+            String[] tokenString = string.split("&");
+            String token = tokenString[0].split("=")[1];
+            return token;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public GithubUser getUser(String accessToken) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token=" + accessToken)
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String string = response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+        }
+        return null;
+    }
+}
