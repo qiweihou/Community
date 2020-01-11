51114f6c65d88c4c5467f4faa30c610e66d7c5fb
houqiwei <qiwei_hou@163.com>
2020-01-11 17:14:29 +0800

    配集成mybatis

--- src/main/java/com/qiweihou/community/Controller/AuthorizeController.java
+++ src/main/java/com/qiweihou/community/Controller/AuthorizeController.java
@@ -2,12 +2,17 @@ package com.qiweihou.community.Controller;
 
 import com.qiweihou.community.dto.AccessTokenDTO;
 import com.qiweihou.community.dto.GithubUser;
+import com.qiweihou.community.mapper.UserMapper;
+import com.qiweihou.community.model.User;
 import com.qiweihou.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 /**
  * @author qiwei
@@ -29,9 +34,13 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state) {
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
@@ -39,8 +48,21 @@ public class AuthorizeController {
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null) {
+            //登录成功，写cookie和session
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user", githubUser);
+            return "redirect:/";
+        } else {
+            //登录失败，重新登录
+            return "redirect:/";
+        }
     }
 }
---
+++ src/main/java/com/qiweihou/community/mapper/UserMapper.java
@@ -0,0 +1,21 @@
+package com.qiweihou.community.mapper;
+
+import com.qiweihou.community.dto.GithubUser;
+import com.qiweihou.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * @author qiwei
+ * @date 2020/1/11
+ */
+@Mapper
+public interface UserMapper {
+    @Insert("insert into user(name, account_id, token, gmt_create, gmt_modified) values" +
+            " (#{name} ,#{accountId} ,#{token} ,#{gmtCreate} ,#{gmtModified}  )")
+    void insert(User user);
+
+    @Select("select * from user")
+    User getAll();
+}
